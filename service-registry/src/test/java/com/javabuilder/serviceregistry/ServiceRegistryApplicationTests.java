a5b90926e86a64cf44fcdff1e463fb94743b0a95
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2026-06-12 16:55:43 +0700

    first commit

---
+++ service-registry/src/test/java/com/javabuilder/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabuilder.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
